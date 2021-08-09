0eaf9dd26e7cacbc5ada444310171ee888a5394a
NikosGour <nikosgournak@gmail.com>
2021-08-09 08:25:00 +0300

    Added buttons and panels

--- src/dithua/nikosgourn/Main.java
+++ src/dithua/nikosgourn/Main.java
@@ -1,7 +1,6 @@
 package dithua.nikosgourn;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.border.MatteBorder;
 import java.awt.*;
 
@@ -17,49 +16,28 @@ public class Main {
         window.setSize(500,500);
         window.setLayout(null);
 
-        JPanel topleft  = new JPanel();
-        topleft.setBorder(new MatteBorder(0,0,3,3, Color.BLACK));
-        topleft.setBounds(100,100,100,100 );
-        window.add(topleft);
+        JPanel[] panels = new JPanel[9];
+        panels[0] = createPanel(window, 0, 0, 3, 3, 100, 100);
+        panels[1] = createPanel(window, 0, 3, 3, 3, 200, 100);
+        panels[2] = createPanel(window, 0, 3, 3, 0, 300, 100);
+        panels[3] = createPanel(window, 3, 0, 3, 3, 100, 200);
+        panels[4] = createPanel(window, 3, 3, 3, 3, 200, 200);
+        panels[5] = createPanel(window, 3, 3, 3, 0, 300, 200);
+        panels[6] = createPanel(window, 3, 0, 0, 3, 100, 300);
+        panels[7] = createPanel(window, 3, 3, 0, 3, 200, 300);
+        panels[8] = createPanel(window, 3, 3, 0, 0, 300, 300);
 
-        JPanel topmid  = new JPanel();
-        topmid.setBorder(new MatteBorder(0,3,3,3, Color.BLACK));
-        topmid.setBounds(200,100,100,100 );
-        window.add(topmid);
+        JButton but = new JButton();
+        but.setBackground(Color.white);
+        panels[0].setLayout(new BorderLayout());
+        panels[0].add(but);
+    }
 
-        JPanel topright  = new JPanel();
-        topright.setBorder(new MatteBorder(0,3,3,0, Color.BLACK));
-        topright.setBounds(300,100,100,100 );
-        window.add(topright);
-
-        JPanel midleft  = new JPanel();
-        midleft.setBorder(new MatteBorder(3,0,3,3, Color.BLACK));
-        midleft.setBounds(100,200,100,100 );
-        window.add(midleft);
-
-        JPanel midmid  = new JPanel();
-        midmid.setBorder(new MatteBorder(3,3,3,3, Color.BLACK));
-        midmid.setBounds(200,200,100,100 );
-        window.add(midmid);
-
-        JPanel midright  = new JPanel();
-        midright.setBorder(new MatteBorder(3,3,3,0, Color.BLACK));
-        midright.setBounds(300,200,100,100 );
-        window.add(midright);
-
-        JPanel botleft  = new JPanel();
-        botleft.setBorder(new MatteBorder(3,0,0,3, Color.BLACK));
-        botleft.setBounds(100,300,100,100 );
-        window.add(botleft);
-
-        JPanel botmid  = new JPanel();
-        botmid.setBorder(new MatteBorder(3,3,0,3, Color.BLACK));
-        botmid.setBounds(200,300,100,100 );
-        window.add(botmid);
-
-        JPanel botright  = new JPanel();
-        botright.setBorder(new MatteBorder(3,3,0,0, Color.BLACK));
-        botright.setBounds(300,300,100,100 );
-        window.add(botright);
+    private static JPanel createPanel(JFrame window, int top, int left, int bottom, int right, int xOffset, int yOffset) {
+        JPanel panel = new JPanel();
+        panel.setBorder(new MatteBorder(top, left, bottom, right, Color.BLACK));
+        panel.setBounds(xOffset, yOffset, 100, 100);
+        window.add(panel);
+        return panel;
     }
 }
